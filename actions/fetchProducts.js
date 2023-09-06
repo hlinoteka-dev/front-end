7f1c56139aaea35eaedce8270da8d29c6b691e2f
Hristo Koev <iamhristokoev@gmail.com>
2023-09-06 12:32:08 +0200

    event fetch no store fix

--- actions/fetchProducts.js
+++ actions/fetchProducts.js
@@ -4,7 +4,7 @@ export async function fetchProducts(page, filter) {
 	const apiUrl = `http://localhost:3000/api/products?page=${page}&filter=${filter}`
 
 	try {
-		const response = await fetch(apiUrl, { next: { tags: ['products'] } }, { cache: 'no-cache' })
+		const response = await fetch(apiUrl, { next: { tags: ['products'] } }, { cache: 'no-store' })
 		const data = await response.json()
 		return data
 	} catch (error) {
