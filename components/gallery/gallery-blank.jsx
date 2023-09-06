8820378b6d2b9e21d4a258ed753199104a645de5
Hristo Koev <iamhristokoev@gmail.com>
2023-09-06 17:12:48 +0200

    gallery

--- components/gallery/gallery-blank.jsx
+++ components/gallery/gallery-blank.jsx
@@ -1,19 +1,35 @@
+"use client"
 
 import { Dialog, Transition } from '@headlessui/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Button from '../button'
+import { dmSans } from '../fonts'
 
 export default function GalleryBlank({ images, isOpen, setIsOpen }) {
 
 	const [index, setIndex] = useState(0)
 	const [total, setTotal] = useState(images.length)
 
+	useEffect(() => {
+		function handleKeyDown(e) {
+			if (e.key === 'ArrowLeft') {
+				setIndex(index - 1 < 0 ? 0 : index - 1)
+			}
+			if (e.key === 'ArrowRight') {
+				setIndex(index + 1 > total - 1 ? index : index + 1)
+			}
+		}
+		window.addEventListener('keydown', handleKeyDown)
+		return () => window.removeEventListener('keydown', handleKeyDown)
+	}, [index])
+
+
 	return (
 		<Transition appear show={isOpen}>
 			<Dialog as="div" onClose={() => setIsOpen(false)}>
 				<Transition.Child
-					className="fixed inset-0 bg-hlinoteka-dark bg-opacity-50 z-50 transition-opacity"
+					className="fixed inset-0 bg-hlinoteka-dark bg-opacity-90 z-50 transition-opacity"
 					enter="transition ease-out duration-200"
 					enterFrom="opacity-0"
 					enterTo="opacity-100"
@@ -31,28 +47,37 @@ export default function GalleryBlank({ images, isOpen, setIsOpen }) {
 					leaveFrom="opacity-100 translate-y-0"
 					leaveTo="opacity-0 translate-y-4"
 				>
-					<Dialog.Panel className="flex justify-center items-center bg-red-500 p-4">
+					<Dialog.Panel className="flex justify-center items-center">
 						<div>
-							<div className="flex justify-center items-center gap-16">
-								<div>
-									<Button type="icon" onClick={() => { setIndex(index - 1 < 0 ? 0 : index - 1) }}>
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-										</svg>
-									</Button>
+							<div className="mb-4 text-left">
+								<span>{index + 1} / {total}</span>
+							</div>
+							<div className="md:px-8 w-full grid grid-cols-10 items-center">
+								<div className='w-full col-span-1 text-left'>
+									{index - 1 >= 0 && (
+										<Button type="icon" onClick={() => { setIndex(index - 1 < 0 ? 0 : index - 1) }}>
+											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+												<path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+											</svg>
+										</Button>
+									)}
 								</div>
-								<img src={images[index].url} alt={images[index].text} className="max-w-4xl max-h-[56rem] rounded-2xl" />
-								<div>
-									<Button type="icon" onClick={() => { setIndex(index + 1 > total - 1 ? index : index + 1) }}>
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-											<path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-										</svg>
-									</Button>
+								<img src={images[index].url} alt={images[index].text} className="w-full h-full max-w-3xl max-h-[48rem] col-span-8 rounded-2xl" />
+								<div className='w-full col-span-1 text-right'>
+									{index + 1 < total && (
+										<Button type="icon" onClick={() => { setIndex(index + 1 > total - 1 ? index : index + 1) }}>
+											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+												<path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+											</svg>
+										</Button>
+									)}
 								</div>
 							</div>
-							<div>
-							<span><pre>{JSON.stringify(images[index], null, 2)}</pre></span>
-							</div>
+							{images[index].description && (
+								<div className={`mt-4 text-center ${dmSans.className}`}>
+									<span>{images[index].description}</span>
+								</div>
+							)}
 						</div>
 					</Dialog.Panel>
 				</Transition.Child>
