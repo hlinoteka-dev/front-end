844f5abe2763c93e6f48b7e4c0638d804550fb1d
Hristo Koev <iamhristokoev@gmail.com>
2023-09-07 13:59:39 +0200

    nodemailer + loading/error tester

---
+++ actions/layoutTester.js
@@ -0,0 +1,20 @@
+/**
+ * Test loading and error layouts for NextJS's app dir.
+ * @param searchParams
+ * @returns Promise
+ */
+export const layoutTester = async (searchParams) => {
+	if (process.env.NODE_ENV !== 'development') {
+		return;
+	}
+
+	if (typeof searchParams.loading !== 'undefined') {
+		const loading = parseInt(searchParams.loading || '2000');
+		await new Promise((resolve) => setTimeout(resolve, loading));
+	}
+
+	if (typeof searchParams.error !== 'undefined') {
+		const error = searchParams.error || 'Something went wrong!';
+		await new Promise((_resolve, reject) => reject(error));
+	}
+};
--- app/(default)/produkty/page.jsx
+++ app/(default)/produkty/page.jsx
@@ -2,11 +2,16 @@ import ContactForm from "@/components/products/contact-form"
 import Products from "@/components/products/products"
 import Footer from "@/components/footer"
 
+import { layoutTester } from "@/actions/layoutTester"
+
 export const metadata = {
 	title: 'Produkty - Hlinoteka',
 	description: 'V naší nabídce najdete keramické výrobky od českých výrobců. Všechny produkty jsou ručně vyráběné a vyráběné v České republice.',
 }
-export default function Produkty() {
+export default async function Produkty(props) {
+
+	await layoutTester(props.searchParams)
+
 	return (
 		<main className="w-full">
 			<Products />
